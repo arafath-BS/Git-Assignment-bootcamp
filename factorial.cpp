172bdcdc97811bd2ee7008211546eeddddd6a953
arafath-BS <arafath.santo@brainstation-23.com>
2024-04-25 12:05:13 +0600

    factorial code added to the feature-branch

---
+++ factorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    if (number < 0) {
+        cout << "Factorial of negative numbers is not defined." << endl;
+    } else {
+        unsigned long long fact = factorial(number);
+        cout << "Factorial of " << number << " is: " << fact << endl;
+    }
+
+    return 0;
+}
